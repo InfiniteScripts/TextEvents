e2f1d37d5c27af5ff601824cc0a733f96fe7bfa6
Kevin Greene <kevin@infinitescripts.com>
2018-02-04 00:18:03 -0600

    Heroku mongo update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 //mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds223738.mlab.com:23738/heroku_4m2mqgn9'); 
+mongoose.connect('mongodb://infini-heroku:K1282859k@ds223738.mlab.com:23738/heroku_4m2mqgn9'); 
 
 app.use('/', express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: false }));
